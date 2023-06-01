18168a8f30f8b3bcf6b9a91ababd34880ae8e4b8
Yuhei FUJIKAKE <y.fujikake@fd-prod.jp>
2023-06-02 00:35:10 +0900

    feat(*): Pugを使用できるように改修
    Pugを使用するに伴ってディレクトリの構成を変更
    スマホでも動くように調整
    
    closed #3

--- webpack.config.js
+++ webpack.config.js
@@ -1,137 +1,103 @@
 const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const cssInline = false;
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const PugPlugin = require("pug-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const WebpackWatchedGlobEntries = require("webpack-watched-glob-entries-plugin");
-const { htmlWebpackPluginTemplateCustomizer } = require("template-ejs-loader");
-const entries = WebpackWatchedGlobEntries.getEntries(
-  [path.resolve(__dirname, "./src/*.ejs")],
-  {
-    ignore: path.resolve(__dirname, "./src/_*.ejs"),
-  }
-)();
 
-if (cssInline) {
-  styleLoader = "style-loader";
-} else {
-  styleLoader = { loader: MiniCssExtractPlugin.loader };
-}
+// aliases used in sources of pug, scss, js
+const Resolve = {
+  alias: {
+    Pages: path.join(__dirname, "src/pages/"),
+    Images: path.join(__dirname, "src/assets/images/"),
+    Fonts: path.join(__dirname, "src/assets/fonts/"),
+    Styles: path.join(__dirname, "src/assets/styles/"),
+    Scripts: path.join(__dirname, "src/assets/scripts/"),
+  },
+};
 
-// 複数htmlを出力させる
-const htmlGlobPlugins = (entries, srcPath) => {
-  return Object.keys(entries).map(
-    (key) =>
-      new HtmlWebpackPlugin({
-        //出力ファイル名
-        filename: `${key}.html`,
-        //テンプレートに使用するファイルを指定 htmlの場合は.html ejsの場合は.ejs
-        template: htmlWebpackPluginTemplateCustomizer({
-          templatePath: `${srcPath}/${key}.ejs`,
-        }),
-        // <script> ~ </script> タグの挿入位置
-        inject: "body",
-        // スクリプト読み込みのタイプ
-        scriptLoading: "defer",
-      })
-  );
+const RulesPug = {
+  test: /\.pug$/,
+  loader: PugPlugin.loader,
+};
+
+const RulesJavaScript = {
+  test: /\.js$/,
+  use: [
+    {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  ],
+};
+
+const RulesStyle = {
+  test: /\.(css|sass|scss)$/,
+  use: [
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: [
+            ["autoprefixer", { grid: true }],
+            ["cssnano", { preset: "default" }],
+          ],
+        },
+      },
+    },
+    "sass-loader",
+  ],
+};
+
+const RulesImages = {
+  test: /\.(png|svg|jpe?g|webp)$/i,
+  type: "asset/resource",
+  include: /assets[\\/]images/,
+  generator: {
+    filename: "assets/img/[name].[hash:8][ext]",
+  },
 };
 
 module.exports = {
   mode: "development",
-
   devServer: {
-    static: path.resolve(__dirname, "src"),
-    port: 8080,
+    static: {
+      directory: path.join(__dirname, "docs"),
+    },
+    compress: true,
+    watchFiles: {
+      paths: ["src/**/*.*"],
+      options: {
+        usePolling: true,
+      },
+    },
     open: true,
   },
   devtool: "source-map",
-  entry: "./src/assets/js/index.js",
+  entry: {
+    index: "./src/pages/home/index.pug",
+  },
   output: {
     path: path.resolve(__dirname, "docs"),
-    filename: "assets/js/index.js",
-    assetModuleFilename: "assets/images/[name][ext]",
+    publicPath: "/",
   },
   module: {
-    rules: [
-      // ejsの設定
-      {
-        test: /\.ejs$/i,
-        use: ["template-ejs-loader"],
-      },
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        ],
-      },
-      // scssの設定
-      {
-        test: /\.(sass|scss)$/,
-        use: [
-          styleLoader,
-          {
-            loader: "css-loader",
-            options: {
-              url: false,
-              sourceMap: true,
-              importLoaders: 1,
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  ["autoprefixer", { grid: true }],
-                  ["cssnano", { preset: "default" }],
-                ],
-              },
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: { sourceMap: true },
-          },
-        ],
-      },
-      // imagesの設定
-      {
-        test: /\.(png|jpg|gif)$/i,
-        generator: {
-          filename: "images/[name][ext][query]",
-        },
-        type: "asset/resource",
-      },
-    ],
+    rules: [RulesPug, RulesJavaScript, RulesStyle, RulesImages],
   },
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "src"),
-    },
-  },
+  resolve: Resolve,
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: "assets/css/style.css" }),
-    ...htmlGlobPlugins(entries, "./src"),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: `${path.resolve(__dirname, "src")}/assets/images/`,
-          to: `${path.resolve(__dirname, "dist")}/assets/images/`,
-        },
-      ],
+    new PugPlugin({
+      js: {
+        filename: "assets/js/[name].[contenthash:8].js",
+      },
+      css: {
+        filename: "assets/css/[name].[contenthash:8].css",
+      },
     }),
   ],
 };
